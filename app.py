73720e918ca1fabb5ddbfe0aba5af56332a1ef09
rajeshchalla10 <158029282+rajeshchalla10@users.noreply.github.com>
2025-05-28 19:49:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 from flask import Flask, render_template, request
-import sklearn
-import pandas as pd
-import pickle
-import numpy as np
-import math
 
 
 app = Flask(__name__)
@@ -15,13 +10,3 @@ def index():
 
     return render_template('index.html',locations=locations,area_type=area_type)
 
-@app.route('/predict', methods=['GET','POST'])
-
-
-
-
-  
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port= 5500)
