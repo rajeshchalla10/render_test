fd65b2d1a3514426fd36545f81706af91704d97d
rajeshchalla10 <158029282+rajeshchalla10@users.noreply.github.com>
2025-05-31 19:54:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import sklearn
+import pandas as pd
+import pickle
+import numpy as np
+import math
+
+
 app = Flask(__name__)
+data = pd.read_csv('Bengaluru_House_Cleaned_Data.csv')
+pipe = pickle.load(open('RidgeModel.pkl','rb'))
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    
+   
+    locations = sorted(data['location'].unique())
+    area_type = sorted(data['area_type'].unique())
+
+    return render_template('index_test.html',locations=locations,area_type=area_type)
+
